5cd752418ac5e3918270bb84219e8be9d85df8d5
Kawandeep Virdee <whichlight@gmail.com>
2016-01-14 01:13:50 -0500

    intitial commit

---
+++ sketch.js
@@ -0,0 +1,302 @@
+//touch interaction var
+var m= {x:0, y:0};
+var mi= {x:0, y:0};
+
+
+var initialize = true;
+var grow;
+var g;
+var offset = 0;
+var speed = -0.5;
+var ydiff=0;
+var outVal=0;
+
+var note =50;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grow = windowHeight;
+  background(0);
+  initSynth();
+
+
+  //disable default touch events for mobile
+  var el = document.getElementsByTagName("canvas")[0];
+  el.addEventListener("touchstart", pdefault, false);
+  el.addEventListener("touchend", pdefault, false);
+  el.addEventListener("touchcancel", pdefault, false);
+  el.addEventListener("touchleave", pdefault, false);
+  el.addEventListener("touchmove", pdefault, false);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function pdefault(e){
+  e.preventDefault()
+}
+
+function draw() {
+  update();
+  render();
+}
+
+function update(){
+  //normalize interaction
+  m.x = max(touchX, mouseX);
+  m.y = max(touchY, mouseY);
+  m.pressed = mouseIsPressed || touchIsDown;
+
+ // fsynth.setPitch(map(outVal,1,0,note*4*2/3,note*2/3));
+  fsynth.lfo.frequency.value=(map(outVal,1,0,note*10.7,note*10.4));
+  fsynth.setVolume(map(outVal,1,0,0.4,0));
+
+  dsynth.setPitch(map(outVal,1,0,note*4*2,note*4*2.1));
+  dsynth.lfo.frequency.value=(map(outVal,1,0,note*4.01,note*4.4));
+  dsynth.setVolume(map(outVal,1,0,0.4,0));
+
+ // this.lfo.frequency.value = this.lfo_pitch_freq;
+
+//  gsynth.setPitch(map(outVal,1,0,note,note/2));
+  gsynth.setVolume(map(outVal,1,0,0.1,0.2));
+
+
+  esynth.setPitch(map(outVal,1,0,note*4.5,note*4));
+  esynth.setVolume(map(outVal,1,0,0.1,0.3));
+
+
+}
+
+
+function render(){
+  var ratio_angle = windowWidth/3.8;
+  var maxGrow = windowHeight + ratio_angle;
+  var minGrow = windowHeight - ratio_angle;
+
+  outVal = (grow-minGrow)/(maxGrow-minGrow);
+
+  if(frameCount%10==0){console.log(outVal);}
+
+  if(!m.pressed){
+    if(initialize==false){
+      console.log('test');
+      ydiff=0;
+    }
+    grow-=1;
+    initialize=true;
+
+
+  }
+
+  if(m.pressed){
+    if(initialize){
+     mi.y = m.y;
+     initialize=false;
+    }
+    ydiff = (mi.y-m.y);
+
+    if(speed<10){
+      if(grow<maxGrow && grow>minGrow){
+        if (ydiff < ratio_angle*2){
+          grow= grow - ydiff;
+          mi.y = m.y;
+        }
+      }else{
+        if(grow>=maxGrow){
+          if(ydiff<=0){
+            offset=-1*ydiff;
+          }else{
+            grow= grow - ydiff;
+            mi.y = m.y;
+          }
+        }
+        if(grow<=minGrow){
+           if(ydiff>=0){
+            offset=-ydiff;
+          }else{
+            grow= grow - ydiff;
+            mi.y = m.y;
+          }
+
+        }
+      }
+
+    }
+
+    /*
+    if(grow<=minGrow){
+      speed--;
+    }
+    if(grow>=maxGrow){
+      speed++;
+    }
+    */
+
+
+
+  }
+
+
+  background(0);
+  stroke(255);
+  strokeWeight(10);
+
+
+  for(var i=-40; i<windowHeight/20+30; i+=1){
+    // line(i, 0, i, windowHeight);
+    strokeWeight(Math.pow((i+40+(ydiff/20))/20,2));
+    stroke(255);
+    line(0, windowHeight- 20*i + offset, windowWidth/2,grow-20*i + offset);
+    strokeWeight(Math.pow((i+40+(ydiff/20))/20,2));
+    stroke(255);
+    line(windowWidth, windowHeight- 20*i+offset, windowWidth/2,grow-20*i+offset);
+  }
+
+
+  if(grow>maxGrow){grow=maxGrow;}
+  if(grow<minGrow){grow=minGrow;}
+
+  if(grow==maxGrow){
+  }
+
+  if(grow> minGrow){
+  }
+
+  offset+=speed;
+  offset%=20;
+}
+
+
+
+function Drone(f, vol, lfo_rate, lfo_pitch_freq){
+  this.filter;
+  this.gain;
+  this.osc;
+  this.played = false;
+  this.volume = vol;
+  this.pitch = f;
+  this.lfo_rate = lfo_rate;
+  this.lfo_pitch_freq = lfo_pitch_freq;
+  this.buildSynth();
+  this.play();
+}
+
+
+Drone.prototype.buildSynth = function(){
+  this.osc = context.createOscillator(); // Create sound source
+  this.osc.type = "sawtooth";
+  this.osc.frequency.value = this.pitch;
+
+
+  //lfo to osc
+  this.lfo = context.createOscillator(); // Create sound source
+  this.lfo.type = "sine";
+  this.lfo.frequency.value = this.lfo_pitch_freq;
+  this.lfoGain = context.createGain(); // Create sound source
+  this.lfoGain.gain.value = 200;
+  this.lfo.connect(this.lfoGain);
+  this.lfoGain.connect(this.osc.frequency);
+  this.lfo.start(0);
+
+  this.filter = context.createBiquadFilter();
+  this.filter.type = "lowpass";
+  this.filter.frequency.value = 500;
+
+  //lfo to filter
+  this.filtlfo = context.createOscillator();
+  this.filtlfo.type ="sine";
+  this.filtlfo.frequency.value = this.lfo_rate;
+  this.filtlfoGain = context.createGain();
+  this.filtlfoGain.gain.value = 50;
+  this.filtlfo.connect(this.filtlfoGain);
+  this.filtlfoGain.connect(this.filter.frequency);
+  this.filtlfo.start(0);
+
+
+
+
+  this.gain = context.createGain();
+  this.gain.gain.value = this.volume;
+
+
+
+  //decay
+  this.osc.connect(this.filter); // Connect sound to output
+  this.filter.connect(this.gain);
+  this.gain.connect(context.destination);
+}
+
+Drone.prototype.setPitch = function(p){
+  this.osc.frequency.value = p;
+}
+
+Drone.prototype.setFilter = function(f){
+  this.filter.frequency.value = f;
+}
+
+Drone.prototype.setVolume= function(v){
+  this.gain.gain.value = v;
+  this.volume = v;
+}
+
+Drone.prototype.play = function(){
+  this.osc.start(0); // Play instantly
+}
+
+Drone.prototype.stop = function(){
+    this.setVolume(0);
+    this.osc.disconnect();
+    return false;
+}
+
+
+function initSynth(){
+  try{
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    context = new AudioContext();
+
+    var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
+    if (iOS) {
+        window.addEventListener('touchend', function() {
+            var buffer = context.createBuffer(1, 1, 22050);
+            var source = context.createBufferSource();
+            source.buffer = buffer;
+            source.connect(context.destination);
+            source.start(0);
+            $("#initialize").remove();
+        }, false);
+    }
+  }
+  catch (err){
+    alert('web audio not supported');
+  }
+
+  if(typeof(context)!="undefined"){
+
+    gsynth = new Drone(note, 0.7, note*2.001, note*2.008);
+    gsynth.setFilter(300);
+    gsynth.setVolume(0);
+
+
+    esynth = new Drone(note*4, 0.6, note*8.001, note*2.001);
+    esynth.setFilter(400);
+    esynth.setVolume(0);
+
+
+
+
+     dsynth = new Drone(note*4*2, 0.2, 0, note*4.001);
+    dsynth.lfoGain.gain.value = 1000;
+     dsynth.setFilter(500);
+    dsynth.setVolume(0);
+
+     fsynth = new Drone(note*4*1/3, 0.2, 200, note*4.601);
+  fsynth.lfoGain.gain.value = 500;
+      fsynth.setFilter(500);
+    fsynth.setVolume(0);
+
+  }
+}
+
